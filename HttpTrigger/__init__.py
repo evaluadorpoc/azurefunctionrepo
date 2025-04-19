2e5d8a719031e873d50f73c2bcc3588e59348dfc
evaluadorpoc <evaluadorpoc@outlook.com>
2025-04-19 11:19:51 -0500

    🔧 PoC3 - Azure Function manual inicial

---
+++ HttpTrigger/__init__.py
@@ -0,0 +1,6 @@
+﻿import azure.functions as func
+import logging
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("✅ Webhook recibido")
+    return func.HttpResponse("Hello from manual Azure Function!", status_code=200)
